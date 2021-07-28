3faf832707ebe1c6288c38f5d4ff864714a6c330
Ceres Luisa Martins <ceres_luisa@hotmail.com>
2021-07-28 16:02:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -68,23 +68,23 @@ btnLevel.onclick = function() {
 }
 
 function criarBG() {
-    context.fillStyle = 'lightgreen';
+    context.fillStyle = '#ff9bae';
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
     for(i=0; i < snake.length; i++){
-        context.fillStyle = 'green';
+        context.fillStyle = 'gray';
         context.fillRect(snake[i].x, snake[i].y, box, box)        
-        context.strokeStyle = 'lightgreen';
+        context.strokeStyle = '#ff9bae';
         context.strokeRect(snake[i].x, snake[i].y, box, box)
     }
 }
 
 function drawFood() {
-    context.fillStyle = "red";
+    context.fillStyle = '#ff4265';
     context.fillRect(food.x, food.y, box, box);
-    context.strokeStyle = 'lightgreen';
+    context.strokeStyle = '#ff9bae';
     context.strokeRect(food.x, food.y, box, box)
 }
 
